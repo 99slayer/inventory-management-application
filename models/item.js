bef186d7313bbc0b416d533b1b20679d093aa6e0
99slayer <21979862+99slayer@users.noreply.github.com>
2023-11-08 16:34:43 -0700

    Create subschema in item model file
    Created the SizeStockSchema to hold data on which sizes of an item are
    in stock, and how many.

--- models/item.js
+++ models/item.js
@@ -2,12 +2,24 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+// Use n value if the item does not have multiple sizes.
+const SizeStockSchema = new Schema({
+  n: { type: Number },
+  s: { type: Number },
+  m: { type: Number },
+  l: { type: Number },
+  xl: { type: Number },
+});
+
 const ItemSchema = new Schema({
   name: { type: String },
   description: { type: String },
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
   price: { type: Number },
-  number_in_stock: { type: Number },
+  sizes_in_stock: {
+    type: SizeStockSchema,
+    default: {}
+  },
 });
 
 ItemSchema.virtual('url').get(function () {
