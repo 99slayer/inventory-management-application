17218d36127e6e2f3f42f820b8fb15627e3c2531
99slayer <21979862+99slayer@users.noreply.github.com>
2023-11-12 17:51:07 -0700

    Fill out list and detail controller functions

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,12 +2,44 @@ const Category = require('../models/category');
 const Item = require('../models/item');
 const asyncHandler = require('express-async-handler');
 
+// Display list of all categories.
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send('placeholder');
+  const [categories, items] = await Promise.all([
+    Category.find({}, 'name')
+      .sort({ name: 1 })
+      .exec(),
+    Item.find({})
+      .populate('category')
+      .exec()
+  ])
+  res.render(
+    'category_list',
+    {
+      title: 'Category List',
+      category_list: categories,
+      item_list: items,
+    }
+  );
 });
 
+// Display details of a single category.
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send('placeholder');
+  const [category, categoryItems] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }).exec(),
+  ]);
+
+  if (category === null) {
+    const err = new Error('Category not found.');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('category_detail', {
+    title: 'Category Detail',
+    category: category,
+    category_items: categoryItems,
+  });
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,12 +2,28 @@ const Item = require('../models/item');
 const Category = require('../models/category');
 const asyncHandler = require('express-async-handler');
 
+// Display list of all items.
 exports.item_list = asyncHandler(async (req, res, next) => {
-  res.send('placeholder');
+  const items = await Item.find({}, 'name category')
+    .populate('category')
+    .sort('category')
+    .exec();
+  res.render('item_list', { title: 'Item List', item_list: items });
 });
 
+// Display details of a single item.
 exports.item_detail = asyncHandler(async (req, res, next) => {
-  res.send('placehold');
+  const item = await Item.findById(req.params.id)
+    .populate('category')
+    .exec();
+
+  if (item === null) {
+    const err = new Error('Item not found.');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('item_detail', { title: 'Item Detail', item: item });
 });
 
 exports.item_create_get = asyncHandler(async (req, res, next) => {
